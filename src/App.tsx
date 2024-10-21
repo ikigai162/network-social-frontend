3c6dd832d77252ab3c8c4ad361771ecae62b7253
alexandru_plinschi <top@global.com>
2024-10-21 20:40:47 +0300

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button, ButtonGroup } from "@nextui-org/button"
+
+const App = () => {
+  return (
+    <div>
+      <Button color="primary">Button</Button>
+    </div>
+  )
+}
+
+export default App
